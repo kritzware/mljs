3d59558fc36795d1632d229d880c3faad775a14a
LouisBluhm <suspiciouspotato@gmail.com>
2018-04-01 22:53:00 +0100

    Removed previous build.

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Matrix } from './lib/Matrix';
-declare const _default: {
-    Matrix: typeof Matrix;
-};
-export default _default;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Matrix_1 = require("./lib/Matrix");
-exports.default = {
-    Matrix: Matrix_1.Matrix
-};
